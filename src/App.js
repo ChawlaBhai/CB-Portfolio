d9cb9cccae8d8857781d6eb68c2c91fd9b89d8bd
Chawla Bhai <sahajsadhu@gmail.com>
2021-05-27 15:18:11 +0530

    2. AboutSection Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
+import GloabalStyle from "./components/GlobalStyle";
 
 function App() {
     return (
         <div className="App">
+            <GloabalStyle />
             <AboutUs />
         </div>
     );
